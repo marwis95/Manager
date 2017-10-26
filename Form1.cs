8ebf6e7e40f2583432626016329f526d0dcd9213
Marek <marwis95@o2.pl>
2017-10-26 15:01:09 +0200

    Refaktoryzacja

--- Form1.cs
+++ Form1.cs
@@ -52,19 +52,19 @@ namespace WindowsFormsApplication1
             //========================Rozpoznawanie wersji systemu===========================
 
             
+
             using (StreamReader reader = new StreamReader("config.ini")) {
             richTextBox1.Text = reader.ReadToEnd();
             }
 
+            //========================Ladowanie pliku config.ini=============================
+
             int j=0;
-           // string[] tab_klucze;
             string[] tab = richTextBox1.Lines;
-            //MessageBox.Show(tab.Length.ToString());
 
 
             for (int i = 0; i < tab.Length; i++){
                 if ((tab[i].Contains("[")) && (tab[i].Contains("]"))){
-                    //MessageBox.Show(tab[i]);
                     richTextBox2.AppendText(tab[i] + "\n");
                     j++;
                 }
@@ -72,8 +72,9 @@ namespace WindowsFormsApplication1
 
             string[] tab_klucze = richTextBox2.Lines;
 
+            //========================Tworzenie tablicy z nazwami dostepnych kluczy=========
+
             var MyIni = new IniFile("config.ini");
-            //richTextBox1.Text = MyIni.Read("Nazwa", "Ukrywanie");
 
             string section = "";
             string item = "";
@@ -83,11 +84,15 @@ namespace WindowsFormsApplication1
                 section = tab_klucze[i].Substring(1,tab_klucze[i].Length-2);
                 item = MyIni.Read("Nazwa", section);
                 if ((MyIni.Read("System", section) == windows) || (MyIni.Read("System", section) == "Uniwersalny"))
-                {
+                {//Dodawanie do listy checkbox tylko tych opcji które pasują pod dany system opracyjny
                     sectionList.Add(section);
                     checkedListBox1.Items.Add(item);
                 }
             }
+
+
+            //=========================Analiza pliku config.ini===========================
+
         }
 
         
@@ -96,24 +101,24 @@ namespace WindowsFormsApplication1
         {
             var MyIni = new IniFile("config.ini");
             foreach (object item in checkedListBox1.Items)
-            {
+            {//Przelatuje w pętli po kolei przez każdą opcję w checkbox list
                 if (checkedListBox1.GetItemCheckState(checkedListBox1.Items.IndexOf(item)).ToString().Equals("Checked"))
-                {
+                {//Jeżeli dana opcja jest zaznaczona to bede wprowadzał ją do systemu
                     foreach (string section in sectionList)
                     {
                         if (MyIni.Read("Nazwa", section).Equals(checkedListBox1.GetItemText(item)))
-                        {
+                        {//Wyszukuję w pliku config zbioru polecen jakie należy wykonać aby wprowadzić zmianę o podanej nazwie
                             int ileS = 1, ileN = 1, ileW = 1;
                             while(MyIni.KeyExists("On_sciezka_" + ileS, section))
-                            {
+                            {//Ile ścieżek
                                 ileS++;
                             }
                             while (MyIni.KeyExists("On_nazwa_" + ileN, section))
-                            {
+                            {//Ile nazw
                                 ileN++;
                             }
                             while (MyIni.KeyExists("On_wartosc_" + ileW, section))
-                            {
+                            {//Ile wartosci
                                 ileW++;
                             }
                             if(ileS == ileN && ileS == ileW && ileS > 1)
@@ -125,7 +130,7 @@ namespace WindowsFormsApplication1
                                     string wartosc = MyIni.Read("On_wartosc_" + i, section);
                                     Registry.SetValue(sciezka, nazwa, wartosc);
                                 }
-                            }
+                            }//===============Wprowadzanie zmian do rejestru=====================
                             else
                             {
                                 MessageBox.Show("Błąd pliku konfiguracyjnego");
@@ -161,7 +166,7 @@ namespace WindowsFormsApplication1
                                     string wartosc = MyIni.Read("Off_wartosc_" + i, section);
                                     Registry.SetValue(sciezka, nazwa, wartosc);
                                 }
-                            }
+                            }//======Wycofywanie zmian z rejestru (mechanizm działania taki sam jak wprowadzanie)=========
                             else
                             {
                                 MessageBox.Show("Błąd pliku konfiguracyjnego");
@@ -184,6 +189,7 @@ namespace WindowsFormsApplication1
             cmd.StandardInput.Flush();
             cmd.StandardInput.Close();
             cmd.WaitForExit();
+            //================================Resetowanie explorer exe========================================
         }
 
         private void button2_Click(object sender, System.EventArgs e)
@@ -197,11 +203,13 @@ namespace WindowsFormsApplication1
             {
                 tab_do_zapisu[i] = itemChecked.ToString();
                 i++;
-            }
+            }//listowanie zaznaczonych elementów
+
+
             SaveFileDialog save = new SaveFileDialog();
 
             save.FileName = Environment.MachineName + "_" + DateTime.Now.Day + "." + DateTime.Now.Month + "." + DateTime.Now.Year + ".ini";
-
+            //Tworzenie nazwy (nazwa komputera + data) pliku ini
             save.Filter = "Ini File | *.ini";
 
             if (save.ShowDialog() == DialogResult.OK)
@@ -228,6 +236,7 @@ namespace WindowsFormsApplication1
                     writer.Dispose();
                 writer.Close();
              }
+            //=============================Tworzenie pliku z wlasnymi kluczami=====================
 
 
 
@@ -242,13 +251,13 @@ namespace WindowsFormsApplication1
             {
                 System.IO.StreamReader sr = new
                    System.IO.StreamReader(openFileDialog1.FileName);
-                //MessageBox.Show(sr.ReadToEnd());
                 richTextBox3.Text = sr.ReadToEnd();
                 sr.Close();
             }
 
+            //==============================Wczytywanie klucza wlasnego============================
+
             var MyIni = new IniFile(openFileDialog1.FileName);
-            //MessageBox.Show(MyIni.Read("System", "Info"));
             
 
             if (MyIni.Read("System", "Info").Equals(windows))
@@ -264,9 +273,6 @@ namespace WindowsFormsApplication1
                 if ((tab[i].Contains("[")) && (tab[i].Contains("]")) && (tab[i] != "[Info]"))
                 {
                     temp = tab[i].Substring(1, tab[i].Length - 2);
-                    //MessageBox.Show(temp);
-                    //MessageBox.Show(checkedListBox1.Items.Count.ToString());
-                    //MessageBox.Show("item" + checkedListBox1.Items.IndexOf("Ukryj").ToString());
 
                     for (int j = 0; j < tab.Length; j++){
                         if (checkedListBox1.Items.IndexOf(temp) >= 0)
@@ -276,7 +282,7 @@ namespace WindowsFormsApplication1
                     }
 
                 }
-            }
+            }//=============================Zaznaczanie opcji w checklist====================================
 
             }else{
                 if (MyIni.KeyExists("System", "Info") == true)
@@ -303,7 +309,7 @@ namespace WindowsFormsApplication1
                     checkedListBox1.SetItemChecked(i, false);
                 }
                 b = !b;
-            }
+            }//=======================Odznacz wszystko======================
             else
             {
                 for (int i = 0; i < checkedListBox1.Items.Count; i++)
@@ -311,7 +317,7 @@ namespace WindowsFormsApplication1
                     checkedListBox1.SetItemChecked(i, true);
                 }
                 b = !b;
-            }
+            }//=======================Zaznacz wszystko======================
             
         }
     }
